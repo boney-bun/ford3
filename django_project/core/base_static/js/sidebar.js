8308d5f33910ab0fe7ccfddc76dcf63d96c907cf
Boney Bun <boney.bun@gmail.com>
2019-04-15 15:16:33 +0700

    fix sidebar: hide on edit pages, show up on detail pages

--- django_project/core/base_static/js/sidebar.js
+++ django_project/core/base_static/js/sidebar.js
@@ -1,12 +1,16 @@
 const toggleSidebar = () => {
+    /*
    const sidebar = document.getElementById('sidebar')
    sidebar.classList.toggle('active')
+   */
+    $('#sidebar').toggle();
 }
 
 $(document).ready(function () {
 
         $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
+            // $('#sidebar').toggleClass('active');
+            toggleSidebar();
         });
 
 });
